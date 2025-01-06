Fixed risk normalisation

Scripts/Dectetion.py
@@ -140,8 +140,8 @@ class Detect:
         # print(som)
     def lidar_naar_cell(self):
         for i in range(len(self.lidarpoint)):
-            x_frame =  (self.lidarpoint[i][0]-self.patchxmin)/self.reso
-            y_frame =  (self.lidarpoint[i][1]-self.patchymin)/self.reso
+            x_frame = self._x + self.lidarpoint[i][0]
+            y_frame = self._y + self.lidarpoint[i][1]
             x_frame = int(np.round(x_frame))  # Rounds before conversion.
             y_frame = int(np.round(y_frame))
             if (

Scripts/Risk.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 # This class handles the risk function(s)
 class Risk:
 
@@ -8,19 +8,41 @@ class Risk:
         self.weights = weights
         
 
-    def CalcRisk(self, map, i):
+    def Normalise_and_calc_risks_new(self, maps, i):
         """
         Calculate the total risk as a weighted sum of static_risk, detect_risk, and track_risk.
 
         :param weights: Tuple of three weights (w_static, w_detect, w_track)
         :return: Total calculated risk
         """
-        w_static, w_detect, w_track = self.weights
 
-        for cells in map.grid.grid:
-            for cell in cells:
-                cell.total_risk[i] = w_static * cell.static_risk + w_detect * cell.detect_risk[i] + w_track * cell.track_risk[i]
+        # Retrieve global maxima for visualization scaling
+        maxs_cons = maps[0].get_global_max_timestep(i)
+        maxs_var = maps[1].get_global_max_timestep(i)
+
+        # Calculate the biggest maxima across both simulations
+        maxs = tuple(max(cons, var) for cons, var in zip(maxs_cons, maxs_var))
+        max_total, max_static, max_detect, max_track = [value if value > 0 else 1 for value in maxs]
+        w_s, w_d, w_t = self.weights
+
+        for map in maps:
+            for cells in map.grid.grid:
+                for cell in cells:
+                    cell.static_risk /= max_static
+                    cell.detect_risk[i] /= max_detect
+                    cell.track_risk[i] /= max_track
+                    cell.total_risk[i] = w_s * cell.static_risk + w_d * cell.detect_risk[i] + w_t * cell.track_risk[i]
         
+        max_total_cons = np.max(np.array(maps[0].grid.get_total_risk_matrix(i)))
+        max_total_var = np.max(np.array(maps[1].grid.get_total_risk_matrix(i)))
+        max_total = max(max_total_cons, max_total_var)
+        for map in maps:
+            for cells in map.grid.grid:
+                for cell in cells:
+                    cell.total_risk[i] /= max_total
+
+
+
     def normalise_and_calc_risks(self, map, maxs):
         """
         Normalizes risks and calculates total risk per cell using given weights.